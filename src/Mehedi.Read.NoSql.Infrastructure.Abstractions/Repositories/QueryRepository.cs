3a45b6b2860a56888bb578be24992d0b7677440f
MD. Mehedi Hasan Rabbi <mehedi.rabbi@asa-international.com>
2024-09-04 11:10:57 +0600

    Update parameters and implementations

--- src/Mehedi.Read.NoSql.Infrastructure.Abstractions/Repositories/QueryRepository.cs
+++ src/Mehedi.Read.NoSql.Infrastructure.Abstractions/Repositories/QueryRepository.cs
@@ -18,29 +18,33 @@ public abstract class QueryRepository<TQueryModel, Tkey>(IReadDbContext context)
     where TQueryModel : IQueryModel<Tkey>
     where Tkey : IEquatable<Tkey>
 {
-    //protected readonly Task<IEnumerable<TQueryModel>> Collection = context.GetCollectionAsync<TQueryModel>();
-
-    ///// <summary>
-    ///// Gets a query model by its id.
-    ///// </summary>
-    ///// <param name="id">The id of the query model.</param>
-    ///// <returns>The query model.</returns>
-    //public async Task<TQueryModel?> GetByIdAsync(Tkey id)
-    //{
-    //    return (await context.GetCollectionAsync<TQueryModel>()).Where(x => x.Id.Equals(id)).FirstOrDefault();
-    //}
-    public async Task<IEnumerable<TQueryModel>> GetAllCollectionAsync()
+    /// <summary>
+    /// Get all collections
+    /// </summary>
+    /// <returns></returns>
+    public async Task<(long, IEnumerable<TQueryModel>)> GetAllCollectionAsync()
     {
         return await context.GetAllCollectionAsync<TQueryModel>();
     }
 
+    /// <summary>
+    /// Get by Id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public async Task<TQueryModel?> GetByIdAsync(Tkey id)
     {
         return await context.GetByIdAsync<TQueryModel, Tkey>(id);
     }
 
-    public async Task<IEnumerable<TQueryModel>> GetCollectionAsync(int start = 0, int pageSize = 100)
+    /// <summary>
+    /// Get collection with pagination
+    /// </summary>
+    /// <param name="pageNumber"></param>
+    /// <param name="pageSize"></param>
+    /// <returns></returns>
+    public async Task<(long, IEnumerable<TQueryModel>)> GetCollectionAsync(int pageNumber = 1, int pageSize = 100)
     {
-        return await context.GetCollectionAsync<TQueryModel>(start, pageSize);
+        return await context.GetCollectionAsync<TQueryModel>(pageNumber: pageNumber, pageSize: pageSize);
     }
 }
